a4b7599d42e948ed56215bb3bb6858fde91a3c91
Nii Boye <nii.quartey@ashesi.edu.gh>
2017-01-23 21:41:28 +0000

    'ForMinus'

--- Calculator.java
+++ Calculator.java
@@ -12,17 +12,13 @@ public class Calculator {
       double c = Double.parseDouble(args[1]);
       double d = Double.parseDouble(args[2]);
       a = c + d; 
+      System.out.println(a);
     }
     else if (args[0].equalsIgnoreCase("Subtract")) {
       double x = Double.parseDouble(args[1]);
       double y = Double.parseDouble(args[2]);
       a = x - y;
+      System.out.println(a);
     } 
-    else if (args[0].equalsIgnoreCase("Multiply")) {
-      double m = Double.parseDouble(args[1]);
-      double n = Double.parseDouble(args[2]);
-      a = m * n;
-    }
-    
   }
 }
